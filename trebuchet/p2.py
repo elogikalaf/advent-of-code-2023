1d144a68500ce28ded0521ef8ed99abcc98ece14
elo <rmandanehvash@gmail.com>
2023-12-02 11:00:29 +0330

    to complete day 1

--- trebuchet/p2.py
+++ trebuchet/p2.py
@@ -7,21 +7,26 @@ with open('p1.txt') as f:
 
 num_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 inputDigits = []
+finalDigits = []
 lineString = ''
+onlyDigits = ''
 for line in input:
     for char in line:
-        lineString+=char
-        for num in list(num_dict.keys()):
-            if lineString.endswith(num):
-                lineString = lineString.replace(num, f"{str(num_dict[num])}{num[-1]}")
-    inputDigits.append(lineString.strip())
+        if char.isdigit():
+            onlyDigits+=char
+        else:
+            lineString+=char
+            for num in (num_dict.keys()):
+                if num in lineString:
+                    onlyDigits+=str(num_dict[num])
+                    lineString = lineString[-1]
+    finalDigits.append(onlyDigits)
     lineString = ''
-finalDigits = []
-for fixedLine in inputDigits:
-    onlyDigits = [char for char in fixedLine if char.isdigit()]
-    finalDigits.append(int(f"{onlyDigits[0]}{onlyDigits[-1]}"))
+    onlyDigits = ''
 
+count = 0
 for i in finalDigits:
-    print(i)
-print(sum(finalDigits))
+    count+=int((f"{i[0]}{i[-1]}"))
+
+print(count)
 
